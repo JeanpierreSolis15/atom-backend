5bc72824e085711cf21768e1eae524471fcf65ef
Jeanpierre Ponte Solis <jeanpierre.ponte@auna.org>
2025-07-24 19:02:12 -0500

    feat: new changes

--- src/domain/users/exceptions/user-not-found.exception.ts
+++ src/domain/users/exceptions/user-not-found.exception.ts
@@ -1,6 +1,11 @@
-export class UserNotFoundException extends Error {
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class UserNotFoundException extends HttpException {
   constructor(userId: string) {
-    super(`Usuario con ID ${userId} no encontrado`);
+    super(
+      `Usuario con ID ${userId} no encontrado`,
+      HttpStatus.NOT_FOUND,
+    );
     this.name = 'UserNotFoundException';
   }
 }
